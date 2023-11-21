ac9dbfbd4696e8284b5d0458be5237c9fa675432
Defelo <elodef42@gmail.com>
2023-11-21 11:44:06 +0100

    Add nico

--- hosts/academy/wireguard.nix
+++ hosts/academy/wireguard.nix
@@ -7,9 +7,16 @@
       ips = ["10.23.1.1/24"];
       peers = [
         {
+          # Defelo
           publicKey = "1g/V66LITNSzZD0DDIKt4l3u8/yOGYEOtcArAXMvHyQ=";
           presharedKeyFile = config.sops.secrets."wireguard/psk/defelo".path;
           allowedIPs = ["10.23.1.2/32"];
+        }
+        {
+          # Nico T480
+          publicKey = "EtC01X70ExI7Kvrp5tzE8wWlcbKD/QHg6wIvUB5ewQI=";
+          presharedKeyFile = config.sops.secrets."wireguard/psk/nico".path;
+          allowedIPs = ["10.23.1.3/32"];
         }
       ];
     };
@@ -20,5 +27,6 @@
   sops.secrets = {
     "wireguard/private-key" = {};
     "wireguard/psk/defelo" = {};
+    "wireguard/psk/nico" = {};
   };
 }
--- modules/users.nix
+++ modules/users.nix
@@ -4,5 +4,8 @@
     # Defelo
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0+Dd5FL6zKIxkjJaOb+/7fp5YtePkDdGasYESAl0br"
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCqDljgWk+qK1pHdTZdgFgXcMdizAz7OmGR9fx0yROQ6+Ja7zUxnAxOi0ijOk8HLWrZ9xu/TqKPvF29hndCEJtg="
+
+    # Nico
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE2k27mRS2MmZ+b0QqF7eGonD8pEQE3lqFTLUHkUDK3X"
   ];
 }
