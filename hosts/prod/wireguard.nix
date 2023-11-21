f54686ed4298bc468add2b91338d78d73e6ac45e
Defelo <elodef42@gmail.com>
2023-11-21 19:24:20 +0100

    Refactor wireguard config

--- hosts/prod/wireguard.nix
+++ hosts/prod/wireguard.nix
@@ -1,15 +1,15 @@
 {
   config,
   lib,
-  env,
+  server,
   ...
 }: {
   networking.wireguard = {
     enable = true;
-    interfaces.${env.servers.prod.dev.wireguard} = {
+    interfaces.${server.dev.wireguard} = {
       privateKeyFile = config.sops.secrets."wireguard/private-key".path;
-      listenPort = env.servers.prod.wireguard.port;
-      ips = ["${env.servers.prod.net.wireguard.ip4}/24"];
+      listenPort = server.wireguard.port;
+      ips = ["${server.net.wireguard.ip4}/24"];
       peers =
         lib.mapAttrsToList (name: {
           publicKey,
@@ -19,7 +19,7 @@
           presharedKeyFile = config.sops.secrets."wireguard/psk/${name}".path;
           allowedIPs = ["${ip4}/32"];
         })
-        env.servers.prod.wireguard.peers;
+        server.wireguard.peers;
     };
   };
 
@@ -32,5 +32,5 @@
     // (builtins.listToAttrs (map (name: {
       name = "wireguard/psk/${name}";
       value = {};
-    }) (builtins.attrNames env.servers.prod.wireguard.peers)));
+    }) (builtins.attrNames server.wireguard.peers)));
 }
