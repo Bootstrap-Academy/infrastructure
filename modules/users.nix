f0a655e67ffb67f5beb655df420e009780944af2
Defelo <elodef42@gmail.com>
2023-11-30 16:13:23 +0100

    Add root passwords

--- modules/users.nix
+++ modules/users.nix
@@ -1,3 +1,13 @@
 {
+  config,
+  lib,
+  ...
+}: let
+  setRootPassword = builtins.readDir ../hosts/${config.networking.hostName} ? "secrets.yml";
+in {
   users.mutableUsers = false;
+
+  users.users.root.hashedPasswordFile = lib.mkIf setRootPassword config.sops.secrets."users/root/password".path;
+
+  sops.secrets."users/root/password" = lib.mkIf setRootPassword {neededForUsers = true;};
 }
