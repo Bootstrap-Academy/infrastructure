9946b828f854763d34b77ed7ee767579a8fb6ee6
Defelo <elodef42@gmail.com>
2023-12-16 23:11:58 +0100

    Add sandkasten config

--- hosts/sandkasten/sandkasten.nix
+++ hosts/sandkasten/sandkasten.nix
@@ -9,6 +9,41 @@
     settings = {
       host = "0.0.0.0";
       port = 8000;
+
+      enable_metrics = true;
+
+      program_ttl = 300;
+      prune_programs_interval = 60;
+
+      max_concurrent_jobs = 8;
+
+      base_resource_usage_runs = 20;
+      base_resource_usage_permits = 2;
+
+      compile_limits = {
+        cpus = 1;
+        time = 30; # seconds
+        memory = 1024; # mb
+        tmpfs = 256; # mb
+        filesize = 16; # mb
+        file_descriptors = 256;
+        processes = 256;
+        stdout_max_size = 65536;
+        stderr_max_size = 65536;
+        network = false;
+      };
+      run_limits = {
+        cpus = 1;
+        time = 5; # seconds
+        memory = 512; # mb
+        tmpfs = 256; # mb
+        filesize = 16; # mb
+        file_descriptors = 256;
+        processes = 64;
+        stdout_max_size = 65536;
+        stderr_max_size = 65536;
+        network = false;
+      };
     };
   };
 }
