9898d5743b99688b292f4d8673c60a5fea160e14
Defelo <elodef42@gmail.com>
2023-12-20 20:23:59 +0100

    Exclude internal hosts from sandkasten rate limit

--- hosts/prod/nginx.nix
+++ hosts/prod/nginx.nix
@@ -3,13 +3,21 @@
     enable = true;
 
     appendHttpConfig = ''
-      limit_req_zone $binary_remote_addr zone=sandkasten-public:10m rate=20r/m;
+      geo $sandkasten_public_limit {
+        default 1;
+        ${env.net.internal.net4} 0;
+      }
+      map $sandkasten_public_limit $sandkasten_public_limit_key {
+        0 "";
+        1 $binary_remote_addr;
+      }
+      limit_req_zone $sandkasten_public_limit_key zone=sandkasten_public:10m rate=20r/m;
     '';
     virtualHosts."sandkasten.bootstrap.academy" = {
       forceSSL = true;
       enableACME = true;
       extraConfig = ''
-        limit_req zone=sandkasten-public burst=5 nodelay;
+        limit_req zone=sandkasten_public burst=5 nodelay;
         limit_req_status 429;
       '';
       locations."/" = {
