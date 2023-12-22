b29e85763d56414a94da19079a3f64faec51e182
Defelo <elodef42@gmail.com>
2023-12-22 08:07:54 +0100

    Use shop-ms nixos module on test

--- hosts/test/backend/shop.nix
+++ hosts/test/backend/shop.nix
@@ -1,18 +1,37 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  shop-ms-develop,
+  ...
+}: let
+  ms = "shop";
+in {
+  imports = [shop-ms-develop.nixosModules.default];
+
   academy.backend.microservices.shop = {
     port = 8002;
     database.passwordFile = config.sops.secrets."academy-backend/database/passwords/academy-shop".path;
     redis.database = 2;
-    container = {
-      image = "shop-ms:develop";
-      environmentFiles = [config.sops.secrets."academy-backend/microservices/shop-ms".path];
-      environment = {
+  };
+
+  academy.backend.shop = {
+    enable = true;
+    environmentFiles =
+      config.academy.backend.common.environmentFiles
+      ++ [config.sops.secrets."academy-backend/microservices/shop-ms".path];
+    settings =
+      config.academy.backend.common.environment
+      // {
+        PORT = toString config.academy.backend.microservices.${ms}.port;
+        ROOT_PATH = "/${ms}";
+        REDIS_URL = config.academy.backend.common.environment."${lib.toUpper ms}_REDIS_URL";
+        PUBLIC_BASE_URL = "https://${config.academy.backend.domain}/${ms}";
+
         PAYPAL_BASE_URL = "https://api.sandbox.paypal.com";
         PAYPAL_CLIENT_ID = "AY8tdE7PPpUOVbURYdFvrqsisOiJpggHWnNYphRQjbDPCoPcD3z7XUU067hZ6kf4cH82GwQrAkJnhcqn";
 
         INVOICE_TEST = "True";
       };
-    };
   };
 
   sops.secrets = {
