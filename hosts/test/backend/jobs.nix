2dd6f5e68426a1a6a401f366185835ffa15b6990
Defelo <elodef42@gmail.com>
2023-12-22 08:10:57 +0100

    Use jobs-ms nixos module on test

--- hosts/test/backend/jobs.nix
+++ hosts/test/backend/jobs.nix
@@ -1,13 +1,32 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  jobs-ms-develop,
+  ...
+}: let
+  ms = "jobs";
+in {
+  imports = [jobs-ms-develop.nixosModules.default];
+
   academy.backend.microservices.jobs = {
     port = 8003;
     database.passwordFile = config.sops.secrets."academy-backend/database/passwords/academy-jobs".path;
     redis.database = 3;
-    container = {
-      image = "jobs-ms:develop";
-      environmentFiles = [config.sops.secrets."academy-backend/microservices/jobs-ms".path];
-      environment = {};
-    };
+  };
+
+  academy.backend.jobs = {
+    enable = true;
+    environmentFiles =
+      config.academy.backend.common.environmentFiles
+      ++ [config.sops.secrets."academy-backend/microservices/jobs-ms".path];
+    settings =
+      config.academy.backend.common.environment
+      // {
+        PORT = toString config.academy.backend.microservices.${ms}.port;
+        ROOT_PATH = "/${ms}";
+        REDIS_URL = config.academy.backend.common.environment."${lib.toUpper ms}_REDIS_URL";
+        PUBLIC_BASE_URL = "https://${config.academy.backend.domain}/${ms}";
+      };
   };
 
   sops.secrets = {
