161583a23125f6bfb71bba393354212d2fd831b7
Defelo <mail@defelo.de>
2024-09-09 23:13:00 +0200

    Add defelo restic repo

--- hosts/prod/default.nix
+++ hosts/prod/default.nix
@@ -31,10 +31,27 @@
       repositoryPasswordFile = config.sops.secrets."backup/box/repository-password".path;
       sshKeyFile = config.sops.secrets."ssh/private-key".path;
     };
+    defelo = {
+      repository = "rest:https://backup.defelo.de/academy-prod";
+      repositoryPasswordFile = config.sops.secrets."backup/defelo/repository-password".path;
+      environmentFile = config.sops.templates."backup/defelo".path;
+    };
   };
 
-  sops.secrets = {
-    "ssh/private-key".path = "/root/.ssh/id_ed25519";
-    "backup/box/repository-password" = {};
+  networking.hosts."10.23.1.2" = ["backup.defelo.de"];
+
+  sops = {
+    secrets = {
+      "ssh/private-key".path = "/root/.ssh/id_ed25519";
+      "backup/box/repository-password" = {};
+      "backup/defelo/repository-password" = {};
+      "backup/defelo/rest-password" = {};
+    };
+    templates = {
+      "backup/defelo".content = ''
+        RESTIC_REST_USERNAME=academy-prod
+        RESTIC_REST_PASSWORD=${config.sops.placeholder."backup/defelo/rest-password"}
+      '';
+    };
   };
 }
