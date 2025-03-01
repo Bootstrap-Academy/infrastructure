b7097ed3e3b8dba9d63517b4c27f3895c1197e08
Defelo <mail@defelo.de>
2025-03-01 21:57:41 +0100

    glitchtip: migrate to nixos module

--- hosts/prod/docker-images.nix
+++ hosts/prod/docker-images.nix
@@ -1,12 +1,5 @@
 {
   dockerImages = {
-    glitchtip.meta = {
-      imageName = "docker.io/glitchtip/glitchtip";
-      imageDigest = "sha256:abbe47e470f1e206aae73813d685f1c5c0847ef746b2a4870ebc997e89ad2d9b";
-      sha256 = "03c2aik602iv4jq2w4ahh2gp5qbpmy3a93r96k2ni0r388xzvp2f";
-      finalImageName = "docker.io/glitchtip/glitchtip";
-      finalImageTag = "latest";
-    };
     morpheushelper.meta = {
       imageName = "ghcr.io/pydrocsid/morpheushelper";
       imageDigest = "sha256:0644787b006d52dc3fd7d1789d5d159fd89eacc2393d006f0a379dc295eabeca";
--- hosts/prod/glitchtip.nix
+++ hosts/prod/glitchtip.nix
@@ -1,52 +1,30 @@
-{ config, lib, ... }:
+{
+  config,
+  nixpkgs-glitchtip,
+  system,
+  ...
+}:
+
 let
   port = 8100;
-  redisPort = 63790;
   domain = "glitchtip.bootstrap.academy";
-
-  mkContainer =
-    attrs:
-    config.dockerImages.glitchtip.mkContainer (
-      lib.recursiveUpdate {
-        extraOptions = [
-          "--rm=false"
-          "--restart=always"
-          "--network=host"
-          "--no-healthcheck"
-        ];
-        environmentFiles = [ config.sops.templates."glitchtip/environment".path ];
-        environment = {
-          PORT = toString port;
-          REDIS_URL = "redis://127.0.0.1:${toString redisPort}/0";
-          GLITCHTIP_DOMAIN = "https://${domain}";
-          DEFAULT_FROM_EMAIL = "glitchtip@the-morpheus.de";
-          ENABLE_USER_REGISTRATION = "False";
-          ENABLE_ORGANIZATION_CREATION = "False";
-        };
-        volumes = [ "/persistent/data/glitchtip/uploads:/code/uploads" ];
-      } attrs
-    );
 in
+
 {
-  virtualisation.oci-containers.containers = {
-    glitchtip-web = mkContainer { };
-    glitchtip-worker = mkContainer { cmd = [ "./bin/run-celery-with-beat.sh" ]; };
-    glitchtip-migrate = mkContainer {
-      cmd = [
-        "./manage.py"
-        "migrate"
-      ];
-      volumes = [ ];
-      extraOptions = [
-        "--rm=true"
-        "--restart=no"
-        "--network=host"
-        "--no-healthcheck"
-      ];
+  imports = [ "${nixpkgs-glitchtip}/nixos/modules/services/web-apps/glitchtip.nix" ];
+
+  services.glitchtip = {
+    enable = true;
+    package = nixpkgs-glitchtip.legacyPackages.${system}.glitchtip;
+    inherit port;
+    environmentFiles = [ config.sops.templates."glitchtip/environment".path ];
+    settings = {
+      GLITCHTIP_DOMAIN = "https://${domain}";
+      DEFAULT_FROM_EMAIL = "glitchtip@the-morpheus.de";
+      ENABLE_USER_REGISTRATION = false;
+      ENABLE_ORGANIZATION_CREATION = false;
     };
   };
-
-  systemd.services.podman-glitchtip-migrate.serviceConfig.RemainAfterExit = true;
 
   services.nginx.virtualHosts.${domain} = {
     forceSSL = true;
@@ -61,34 +39,12 @@ in
     };
   };
 
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = [ "glitchtip" ];
-    userPasswords.glitchtip = config.sops.secrets."glitchtip/database-password".path;
-  };
-
-  services.redis.servers.glitchtip = {
-    enable = true;
-    bind = null;
-    port = redisPort;
-    save = [ ];
-    settings.protected-mode = "no";
-  };
-
-  system.activationScripts.initGlitchtipState = ''
-    mkdir -p /persistent/data/glitchtip/uploads
-  '';
-
   sops = {
     secrets = {
-      "glitchtip/database-password" = { };
       "glitchtip/secret-key" = { };
       "glitchtip/smtp-password" = { };
     };
     templates."glitchtip/environment".content = ''
-      DATABASE_URL=postgres://glitchtip:${
-        config.sops.placeholder."glitchtip/database-password"
-      }@127.0.0.1:5432/glitchtip
       SECRET_KEY=${config.sops.placeholder."glitchtip/secret-key"}
       EMAIL_URL=smtp+tls://glitchtip@the-morpheus.de:${
         config.sops.placeholder."glitchtip/smtp-password"
