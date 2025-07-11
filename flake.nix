fe210d52fc12029abab5a7236196bd6763b79354
Defelo <mail@defelo.de>
2025-07-11 17:37:13 +0200

    glitchtip: apply patch to remove symbolic override

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-glitchtip.url = "github:NixOS/nixpkgs/pull/424185/merge";
     deploy-sh.url = "git+https://git.defelo.de/Defelo/deploy-sh";
     sops-nix.url = "github:Mic92/sops-nix";
     nfnix = {
@@ -78,7 +79,7 @@
             ./modules
             {
               nixpkgs.overlays = [
-                (final: prev: { inherit (inputs.nixpkgs-unstable.legacyPackages.${final.system}) glitchtip; })
+                (final: prev: { inherit (inputs.nixpkgs-glitchtip.legacyPackages.${final.system}) glitchtip; })
               ];
             }
           ];
