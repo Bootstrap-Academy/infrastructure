cd4f7b7b542d117ee7653e7ae654a608a6a21418
Defelo <mail@defelo.de>
2025-10-30 17:13:10 +0100

    remove update script from dev shell

--- dev.nix
+++ dev.nix
@@ -1,45 +1,14 @@
-{
-  deploy-sh,
-  pkgs,
-  self,
-  system,
-  ...
-}:
+{ deploy-sh, pkgs, ... }:
+
 pkgs.mkShell {
-  packages =
-    let
-      update = pkgs.writeShellApplication {
-        name = "update";
-        runtimeInputs = builtins.attrValues { inherit (pkgs) nix git openssh; };
-        text = ''
-          if [[ $# -eq 0 ]]; then
-            nix flake update --commit-lock-file
-          else
-            args=()
-            for x in "$@"; do
-              args+=(--update-input "$x")
-            done
-            nix flake lock "''${args[@]}" --commit-lock-file
-          fi
+  packages = builtins.attrValues {
+    inherit (pkgs)
+      dnscontrol
+      nixos-anywhere
+      sops
+      ssh-to-age
+      ;
 
-          if [[ $# -eq 0 ]]; then
-            ssh root@10.23.0.2 fetch-docker-images > hosts/prod/docker-images.nix
-          fi
-        '';
-      };
-    in
-    builtins.attrValues {
-      inherit (pkgs)
-        sops
-        ssh-to-age
-        nixos-anywhere
-        dnscontrol
-        ;
-    }
-    ++ [
-      deploy-sh.packages.${system}.default
-      self.formatter.${system}
-
-      update
-    ];
+    deploy-sh = deploy-sh.packages.${pkgs.system}.default;
+  };
 }
