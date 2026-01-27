c1519686c8cc996886011c97f87a5012fd118256
Defelo <mail@defelo.de>
2026-01-27 22:40:38 +0100

    ssh: update keys for defelo

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -15,8 +15,7 @@
 
   users.users.root.openssh.authorizedKeys.keys = [
     # defelo
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0+Dd5FL6zKIxkjJaOb+/7fp5YtePkDdGasYESAl0br"
-    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCqDljgWk+qK1pHdTZdgFgXcMdizAz7OmGR9fx0yROQ6+Ja7zUxnAxOi0ijOk8HLWrZ9xu/TqKPvF29hndCEJtg="
+    "cert-authority ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICwW+Q4ZyT5JZvXAJrD3rPuz2nsK8J7WBiSW3R+wkvOT"
 
     # nico
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE2k27mRS2MmZ+b0QqF7eGonD8pEQE3lqFTLUHkUDK3X"
