5a920cd57f876ea803a8baba519488d63855d574
TheMorpheus407 <kontakt@the-morpheus.de>
2026-09-11 16:39:31 +0200

    Include the verified legacy XP export correction

--- flake.nix
+++ flake.nix
@@ -13,13 +13,13 @@
     impermanence.url = "github:nix-community/impermanence";
     sandkasten.url = "git+https://radicle.defelo.de/zKBbWZxz73j7BZMbutM7TMMT4v5K.git";
 
-    skills-ms.url = "github:Bootstrap-Academy/skills-ms/5d8b6d588c0431bc2dab6c057605784b7312a9b3";
+    skills-ms.url = "github:Bootstrap-Academy/skills-ms/7561f9b709746c273bee5717c18953d53e29faf4";
     jobs-ms.url = "github:Bootstrap-Academy/jobs-ms/ea946c12f6f2caaa56e4b2e4edc73ce926ccfa2b";
     events-ms.url = "github:Bootstrap-Academy/events-ms/337d3002183c7420a1ebd2bb38474350c36eb7cb";
     challenges-ms.url = "github:Bootstrap-Academy/challenges-ms/6c4a7453b970212c31639048d59baccd87220e29";
     backend.url = "github:Bootstrap-Academy/backend/ba62852fc4d76f8a38c6ce99f46abbaba3714062";
 
-    skills-ms-develop.url = "github:Bootstrap-Academy/skills-ms/5d8b6d588c0431bc2dab6c057605784b7312a9b3";
+    skills-ms-develop.url = "github:Bootstrap-Academy/skills-ms/7561f9b709746c273bee5717c18953d53e29faf4";
     jobs-ms-develop.url = "github:Bootstrap-Academy/jobs-ms/ea946c12f6f2caaa56e4b2e4edc73ce926ccfa2b";
     events-ms-develop.url = "github:Bootstrap-Academy/events-ms/337d3002183c7420a1ebd2bb38474350c36eb7cb";
     challenges-ms-develop.url = "github:Bootstrap-Academy/challenges-ms/6c4a7453b970212c31639048d59baccd87220e29";
