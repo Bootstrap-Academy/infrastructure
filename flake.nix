54cea44794172dc34a3071b7febc6734de020d6d
TheMorpheus407 <kontakt@the-morpheus.de>
2026-09-11 16:28:57 +0200

    Enable new purchases with the agreed 24-hour provision window

--- flake.nix
+++ flake.nix
@@ -17,13 +17,13 @@
     jobs-ms.url = "github:Bootstrap-Academy/jobs-ms/ea946c12f6f2caaa56e4b2e4edc73ce926ccfa2b";
     events-ms.url = "github:Bootstrap-Academy/events-ms/337d3002183c7420a1ebd2bb38474350c36eb7cb";
     challenges-ms.url = "github:Bootstrap-Academy/challenges-ms/6c4a7453b970212c31639048d59baccd87220e29";
-    backend.url = "github:Bootstrap-Academy/backend/2e7477f46e2602f9fe93fa8055b5dd3cb9722f1c";
+    backend.url = "github:Bootstrap-Academy/backend/ba62852fc4d76f8a38c6ce99f46abbaba3714062";
 
     skills-ms-develop.url = "github:Bootstrap-Academy/skills-ms/5d8b6d588c0431bc2dab6c057605784b7312a9b3";
     jobs-ms-develop.url = "github:Bootstrap-Academy/jobs-ms/ea946c12f6f2caaa56e4b2e4edc73ce926ccfa2b";
     events-ms-develop.url = "github:Bootstrap-Academy/events-ms/337d3002183c7420a1ebd2bb38474350c36eb7cb";
     challenges-ms-develop.url = "github:Bootstrap-Academy/challenges-ms/6c4a7453b970212c31639048d59baccd87220e29";
-    backend-develop.url = "github:Bootstrap-Academy/backend/2e7477f46e2602f9fe93fa8055b5dd3cb9722f1c";
+    backend-develop.url = "github:Bootstrap-Academy/backend/ba62852fc4d76f8a38c6ce99f46abbaba3714062";
   };
 
   outputs =
